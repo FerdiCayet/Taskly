8595e10470b9769a4adadf1e2160fb5efa785c7e
Fernando Caetano <fernkndy@gmail.com>
2026-02-13 09:18:49 -0300

    chore: projeto inicial

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="bg-(--pine) m-auto mt-15 p-3.75 flex justify-center flex-col w-200 border-2 rounded-md border-(--dark-green)">
+            <h1 className="text-3xl font-bold text-center subpixel-antialiased font-stretch-extra-expanded cursor-default">Hello World</h1>
+        </div>
+    );
+}
