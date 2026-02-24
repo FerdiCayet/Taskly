472d0c0224e6ff575de34cdb1574eea3de871e36
Fernando Caetano <fernkndy@gmail.com>
2026-02-24 02:50:48 -0300

    feat: adiciona funcionalidade de criação de tarefa

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,18 +1,51 @@
+import { useState } from 'react';
+
 export default function CreateToDo() {
+    const [title, setTitle] = useState<string>('');
+    const [currentCategory, setCurrentCategory] = useState<string>('');
+
+    const isButtonEnabled = title.length > 0 && currentCategory.length > 0;
+
+    const btnTask = () => {
+        console.log('Tarefa criada:', { title, currentCategory });
+        setTitle('');
+        setCurrentCategory('');
+    };
+
     return (
         <div className="flex flex-col gap-2.5 relative m-5">
             <h1 className="text-(--bangladesh-green) font-bold text-2xl cursor-default">Criar Tarefa:</h1>
-            <input className="p-2 bg-(--forest) placeholder:text-(--stone) text-(--anti-flash-white) rounded-md focus:outline-2 outline-offset-2 outline-(--bangladesh-green) transition-all /*delay-150*/ duration-200 ease" type="text" id="task" name="task" placeholder="Digite o Título" />
-            <select className="p-2 bg-(--forest) placeholder:text-[bg-amber-500] text-(--frog) rounded-md focus:outline-2 outline-offset-2 outline-(--bangladesh-green) transition-all duration-200 ease">
-                <option id="category" defaultValue="none" selected disabled>
+            <input
+                className="p-2 bg-(--forest) placeholder:text-(--stone) text-(--anti-flash-white) rounded-md focus:outline-2 outline-offset-2 outline-(--bangladesh-green) transition-all /*delay-150*/ duration-200 ease"
+                type="text"
+                id="task"
+                name="task"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Digite o Título"
+            />
+            <select
+                className="p-2 bg-(--forest) placeholder:text-[bg-amber-500] text-(--frog) rounded-md focus:outline-2 outline-offset-2 outline-(--bangladesh-green) transition-all duration-200 ease"
+                name="selectedCategory"
+                value={currentCategory}
+                onChange={(e) => setCurrentCategory(e.target.value || 'person')}
+            >
+                <option value="" selected disabled>
                     Seleciona uma categoria
                 </option>
-                <option defaultValue="study">Estudos</option>
-                <option defaultValue="person">Pessoal</option>
-                <option defaultValue="job">Trabalho</option>
+                <option value="study">Estudos</option>
+                <option value="person">Pessoal</option>
+                <option value="job">Trabalho</option>
             </select>
             <div className="block relative">
-                <button className="bg-(--frog) hover:bg-(--mountain-meadow) transition text-(--bangladesh-green) font-bold bonderad w-1/2 rounded-md p-2 cursor-pointer">Criar Tarefa</button>
+                <button
+                    className="bg-(--frog) enabled:hover:bg-(--mountain-meadow) transition text-(--bangladesh-green) font-bold bonderad w-1/2 rounded-md p-2 cursor-pointer disabled:cursor-no-drop disabled:opacity-75"
+                    type="button"
+                    disabled={!isButtonEnabled}
+                    onClick={btnTask}
+                >
+                    Criar Tarefa
+                </button>
             </div>
         </div>
     );
